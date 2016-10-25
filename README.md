0f6dc08ed6cc2602a19c343c1b15c759943d7715
HuySokhom <sokhomhuy.corarl@gmail.com>
2016-10-25 15:37:56 +0700

    update master

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# HR Recruitment 
+# Adventure Tourism
 PHP v5.5 
--- includes/configure.example.php
+++ includes/configure.example.php
@@ -1,14 +1,14 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
-  define('HTTP_SERVER', 'http://localhost');
-  define('HTTPS_SERVER', 'http://localhost');
+  define('HTTP_SERVER', 'http://localhost:8080');
+  define('HTTPS_SERVER', 'http://localhost:8080');
   define('ENABLE_SSL', false);
   define('HTTP_COOKIE_DOMAIN', '');
   define('HTTPS_COOKIE_DOMAIN', '');
-  define('HTTP_COOKIE_PATH', '/HR/');
-  define('HTTPS_COOKIE_PATH', '/HR/');
-  define('DIR_WS_HTTP_CATALOG', '/HR/');
-  define('DIR_WS_HTTPS_CATALOG', '/HR/');
+  define('HTTP_COOKIE_PATH', '/advanture/');
+  define('HTTPS_COOKIE_PATH', '/advanture/');
+  define('DIR_WS_HTTP_CATALOG', '/advanture/');
+  define('DIR_WS_HTTPS_CATALOG', '/advanture/');
   define('DIR_WS_IMAGES', 'images/');
   define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
   define('DIR_WS_INCLUDES', 'includes/');
@@ -18,14 +18,14 @@ error_reporting(E_ALL ^ E_DEPRECATED ^ E_NOTICE);
   define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
 
   define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
-  define('DIR_FS_CATALOG', '/var/www/HR/');
+  define('DIR_FS_CATALOG', 'C:wamp\www\advanture/');
   define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
   define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
 
   define('DB_SERVER', 'localhost');
   define('DB_SERVER_USERNAME', 'root');
   define('DB_SERVER_PASSWORD', '');
-  define('DB_DATABASE', 'hrjob');
+  define('DB_DATABASE', 'adventure');
   define('USE_PCONNECT', 'false');
   define('STORE_SESSIONS', 'mysql');
   define('CFG_TIME_ZONE', 'Europe/Berlin');
